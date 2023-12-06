4fd68fde44087c475d39c71aa6b682945795a195
Exequiel Lamagni <exequiellamagni99@gmail.com>
2023-12-06 09:36:35 -0300

    componentes multiples (mod part1)

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// componente de react
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello world</p>
     </div>
   )
 }
-// comando que renderiza su contenido en el elemento div, 
-// definido en el archivo public/index.html, que tiene el valor de id 'root'.
-ReactDOM.render(<App />, document.getElementById('root')) 
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
