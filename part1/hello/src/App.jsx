2b257d97942be1fd72052f0feea760b1b03cc39c
Exequiel Lamagni <exequiellamagni99@gmail.com>
2023-12-09 09:08:33 -0300

    modificacion de hello prueba de manejos de estado y controlador de eventos

--- part1/hello/src/App.jsx
+++ part1/hello/src/App.jsx
@@ -1,29 +1,21 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
-
-
-const Hello = ({name , age}) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import Display from "./Display"
+import Button from "./Button"
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter = {counter}/>
+      <Button handleClick={increaseByOne} text = "plus"/>
+      <Button handleClick={decreaseByOne} text = "minus"/>
+      <Button handleClick={setToZero} text = "zero"/>
     </div>
   )
 }
---
+++ part1/hello/src/Button.jsx
@@ -0,0 +1,11 @@
+const Button = (props) =>{
+    return(
+        <div>
+            <button onClick = {props.handleClick}>
+                {props.text}
+            </button>
+        </div>
+    )
+}
+
+export default Button
